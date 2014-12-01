9f7375f687289aca51309da2e27c0715e679879f
Andrew Snead <andrewcsnead@gmail.com>
2014-12-01 11:30:33 +0000

    basic jquery commands working

--- public/js/interface.js
+++ public/js/interface.js
@@ -1,9 +1,5 @@
 $(document).ready(function(){
   var controller = new Controller();
   var socket = io();
-  controller.onRightClick(socket);
-  controller.onLeftClick(socket);
-  controller.onForwardClick(socket);
-  controller.onReverseClick(socket);
-  controller.onBrakeClick(socket);
+  controller.init(socket);
 });
