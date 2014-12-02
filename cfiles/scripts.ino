7629cbb499ec80cf438bbfddf5b1720a6e19a415
Hadi <hadichalabi@gmail.com>
2014-12-02 18:45:42 +0000

    360 biaaatch

---
+++ cfiles/scripts.ino
@@ -0,0 +1,62 @@
+//360 Spin
+
+#include "Arduino.h"
+
+int pwm_a = 3; //ch1
+int pwm_b = 5; //ch2
+int pwn_c = 6; //ch3
+int pwn_d = 9; //ch4
+
+int dir_a = 2; //ch1
+int dir_b = 4; //ch2
+int dir_c = 7; //ch3
+int dir_d = 8; //ch4
+
+void setup()
+{
+  Serial.begin(57600);
+  
+  delay(1000);
+  
+  pinMode(pwm_a,OUTPUT); 
+  pinMode(pwm_b,OUTPUT); 
+  pinMode(pwn_c,OUTPUT); 
+  pinMode(pwn_d,OUTPUT);
+  
+  pinMode(dir_a,OUTPUT); 
+  pinMode(dir_b,OUTPUT); 
+  pinMode(dir_c,OUTPUT); 
+  pinMode(dir_d,OUTPUT);
+  
+};
+
+void threeSixty(){
+    Serial.println(HIGH);
+    digitalWrite(dir_a,LOW); // turn all motors forward
+    digitalWrite(dir_b,HIGH); 
+    digitalWrite(dir_c,HIGH);
+    digitalWrite(dir_d,LOW);
+    analogWrite(pwm_a,200); // provide power to all motors
+    analogWrite(pwm_b,220);
+    analogWrite(pwn_c,200);
+    analogWrite(pwn_d,220);
+}
+
+void brake(){
+  digitalWrite(pwm_a, LOW);
+  digitalWrite(pwm_b, LOW);
+  digitalWrite(pwn_c, LOW);
+  digitalWrite(pwn_d, LOW);
+}
+
+ void loop (){
+ if(Serial.available()) {
+ byte b = Serial.read();
+ Serial.println(b);
+   if(b == '3') {
+     threeSixty();
+     delay(3600);
+     brake();
+   }
+  }
+ };
